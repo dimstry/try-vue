5526467a3ff1d776aa725d15e9a9904581cbeb3f
GTM-Winter <dimastriana03@gmail.com>
2022-06-12 18:35:00 +0700

    mending react

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,16 @@
 <template>
-  <ul>
-    <li v-for="item in products" :key="item.id">
-      {{ item.nama }} - {{ item.posisi }}
-    </li>
-  </ul>
+  <Header />
 </template>
 
 <script>
+  import Header from "@/components/Header.vue";
   export default {
     name: 'App',
-    data() {
-      return {
-        products: [
-          {
-            id: 1,
-            nama: "Yeji",
-            posisi: "leader"
-          },
-          {
-            id: 2,
-            nama: "Lia",
-            posisi: "main vocal"
-          },
-          {
-            id: 3,
-            nama: "Chaeryong",
-            posisi: "main dancer"
-          },
-          {
-            id: 4,
-            nama: "Ryujin",
-            posisi: "main dancer"
-          },
-          {
-            id: 5,
-            nama: "Yuna",
-            posisi: "maknae"
-          }]
-      };
+    components: {
+      Header,
     },
   };
   </script>
 
   <style>
-    #app {
-      font-family: Avenir, Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      color: #2c3e50;
-      margin-top: 60px;
-    }
   </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Hello Word</h2>
+</template>
+
+<script>
+  export default {
+    names: "Header",
+  };
+  </script>
+
+  <style>
+  </style>
